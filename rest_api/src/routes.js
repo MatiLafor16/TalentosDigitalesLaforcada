bf51e4c741eff8e62bc249c992c8bc306765b550
Mati Laforcada <73038723+MatiLafor16@users.noreply.github.com>
2023-11-09 20:10:36 -0300

    Update routes.js

--- rest_api/src/routes.js
+++ rest_api/src/routes.js
@@ -7,5 +7,5 @@ router.get('/productos', producto.getAll); //traer todos los productos
 router.get('/producto/:id_producto', producto.getOne); //llamar solo un producto por id
 router.post('/producto', producto.add); //añadir nuevo producto
 router.delete('/producto/id_producto', producto.deleteId); //elimina producto por ID
-router.delete('/producto/cod_producto', producto.deleteCod); //elimina producto por ISBN
+router.delete('/producto/cod_producto', producto.deleteCod); //elimina producto por Código de producto
 router.put('/producto', producto.update); //actualiza producto por id
